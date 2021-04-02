c1ed58e687387d603b7b46ab4a28102e21b233bc
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-04-02 16:31:33 -0500

    you can unlike now too

--- client/src/components/buttons/LikeButton.js
+++ client/src/components/buttons/LikeButton.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
 
 const LikeButton = ({user, thoughtToLike}) => {
+
+
 
     const likeThought = userId => {
         axios.post(`/api/like/${thoughtToLike}`, {likes: userId})
@@ -11,10 +13,21 @@ const LikeButton = ({user, thoughtToLike}) => {
         .catch(err => console.log(err))
     }
 
+    // this could be made logical but later
+
+    const unlikeThought = userId => {
+        axios.post(`/api/like/unlike/${thoughtToLike}`, {likes: userId})
+        .then(() => {
+            console.log('also fix this lol')
+        })
+        .catch(err => console.log(err))
+    }
 
     return (
         <div>
 <button onClick={() => likeThought(user._id)}>Like</button>
+<button onClick={() => unlikeThought(user._id)}>Un the like</button>
+
         </div>
     )
 }
--- routes/like.js
+++ routes/like.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const User = require("../models/User");
 const Thought = require('../models/Thought')
 
+
+
+// like a thought
 router.post('/:id', (req, res, next) => {
     const {likes} = req.body
     Thought.findByIdAndUpdate
@@ -14,5 +17,18 @@ router.post('/:id', (req, res, next) => {
             res.json(theLikedThought)
         })
 })
+
+// unlike a thought
+router.post('/unlike/:id', (req, res, next) => {
+    const {likes} = req.body
+   Thought.findByIdAndUpdate(
+       req.params.id, 
+       {$pull: {likes: likes}}, {new: true}
+   ) 
+   .then(unliked => {
+       res.json(unliked)
+   })
+})
+
 
 module.exports = router
