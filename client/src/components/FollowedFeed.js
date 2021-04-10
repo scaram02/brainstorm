ce7f89a7e3a41a17c7fe1552c5b82c724fa5e99f
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-04-09 23:21:20 -0500

    cleaning up following

--- client/src/components/FollowedFeed.js
+++ client/src/components/FollowedFeed.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import DeleteButton from './buttons/DeleteButton'
-import ThoughtList from './ThoughtList2'
+import ThoughtList from './ThoughtList'
 import Loading from './Loading'
 
 
--- client/src/components/ThoughtList.js
+++ client/src/components/ThoughtList.js
@@ -1,43 +1,23 @@
-// import axios from 'axios'
-// import React, {useState, useEffect} from 'react'
-// import {Link} from 'react-router-dom'
-// import DeleteButton from './buttons/DeleteButton'
-// import LikeButton from './buttons/LikeButton'
-// import UnlikeButton from './buttons/UnlikeButton'
-// import Hm from './Hm'
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import DeleteButton from './buttons/DeleteButton'
 
-// const ThoughtList = ({user, thoughtList, message, likes, setLikes}) => {
+import Hm from './ThoughtCard'
 
 
-// // maybe it's better to map out <Thought /> pass the props there and then be able to set the number of likes in sucha way. also cleaner? but idk maybe not
+const ThoughtList2 = ({user, thoughtList, message,}) => {
 
-//     return (
-//         <div>
-// {thoughtList.length? 
-// thoughtList
-//            .map
-//            ((thought) => (
-//              <div key={thought._id} className="thought">
-//             <Link to={`/user/${thought.user.username}`}>{thought.user.username} thought of this</Link>
-//             <Link to={`/thought/${thought._id}`}>
-//             <h1>{thought.thought}</h1>
-//             <h3>{thought.comments.length} comments</h3>
-//             </Link>
 
-//             {(likes.includes(thought._id))? <UnlikeButton setLikes={setLikes} likes={likes} thoughtToLike={thought} user={user} /> : 
-//             <LikeButton setLikes={setLikes} likes={likes} thoughtToLike={thought} user={user} />}
-//             <p>{likes.length} likes...wrong length bc wrong array here</p>
-//             <Hm thought={thought} />
-//            {user.username === thought.user.username && 
-//            <Link to={`/thought/edit/${thought._id}`}>Having second thoughts?</Link>}
+return (
+    <div>
+        {thoughtList.length? thoughtList.map((thought) => (
+            <div key={thought._id}>
+                <Hm thought={thought} user={user}  />
+            </div>
+        )) : <h1>nope</h1>}
+    </div>
+)
+}
 
-//            <DeleteButton thought={thought} user={user}/>
-//             </div>
-          
-//            )) : <h1>{message}</h1>
-//       }  
-//         </div>
-//     )
-// }
-
-// export default ThoughtList
+export default ThoughtList2
