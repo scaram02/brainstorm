e4e68b54016ee014807b17ae617dfb63cca64f35
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-04-16 20:36:22 -0500

    not working but better names

--- client/src/components/actions/UploadPhoto.js
+++ client/src/components/actions/UploadPhoto.js
@@ -5,37 +5,40 @@ const UploadPhoto = ({user}) => {
 
 
     const [photoToUpload, setPhotoToUpload] = useState()
-    const [imageUrl, setImageUrl] = useState('')
+    const [imageUrl, setImageUrl] = useState(null)
 
-    const handleChange = e => {
-        setPhotoToUpload(e.target.files[0]);
-        console.log(e.target.files[0])
-
-        const imageUpload = new FormData()
-        imageUpload.append("imageUrl", e.target.files[0])
-    
-        
-
-    const handleUpload = theImage => {
-        axios.put(`api/user/photo/upload/${user._id}`, theImage)
+    const handleUploadSubmit = imageUrl => {
+        axios.put(`api/user/photo/upload/${user._id}`, {imageUrl: imageUrl})
         .then(res => res.data)
         .catch((err => {
             console.log(err)
         }))
     }
 
-  handleUpload(imageUpload)
-//         .then((res) =>{
-//         //     setImageUrl(res.secure_url)
-//             console.log('resss', res)
-//         })
+    const handleChange = e => {
+        // setPhotoToUpload(e.target.files[0]);
+        console.log(e.target.files[0])
+
+        const imageUpload = new FormData()
+        imageUpload.append("imageUrl", e.target.files[0])
+    
+        
+console.log('imageuplod', imageUpload, 'target fils', e.target.files[0])
+
+  handleUploadSubmit(imageUpload)
+        // .then(() =>{
+    // // set all hte states here
+            // setImageUrl('test test')
+            // console.log('resss', res)
+        // })
       }
 
-
+     
+     
 
     return (
         <div style={{backgroundColor: 'violet'}}>
-            <form>
+            <form onSubmit={handleUploadSubmit}>
            <h1>upload foto</h1>
            <input type="file" onChange={handleChange} />
            </form>
--- routes/user.js
+++ routes/user.js
@@ -9,19 +9,19 @@ const User = require("../models/User");
 // add/edit? bio
 router.put('/edit/:id', (req, res, next) => {
     const {bio} = req.body
-    User.findByIdAndUpdate(req.params.id, bio, (err, user) => {
+    User.findByIdAndUpdate(req.params.id, bio, { new: true }, (err, user) => {
         if (err) return next(err)
         res.json(user)
     })
 })
 
 // upload photo?
-router.put('/photo/upload/:id', (req, res) => {
+router.post('/photo/upload/:id', (req, res) => {
     if (!req.file){
         res.status(404).json({message: 'no photo hath been uplaoded'})
     } else {
-        const {theImage} = req.body
-        User.findByIdAndUpdate(req.params.id, theImage, (err, user) => {
+        const {imageUrl} = req.body
+        User.findByIdAndUpdate(req.params.id, imageUrl, { new: true }, (err, user) => {
             if (err) return next(err)
             res.json(user)
         })
