fe4db67f0b339a9bed67d93de4f2bb26a7cd5609
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-05-07 16:36:17 -0500

    fixed nav slightly and experimenting with backgrounds

--- client/src/components/ThoughtList.js
+++ client/src/components/ThoughtList.js
@@ -3,7 +3,6 @@ import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import DeleteButton from './buttons/DeleteButton'
 
-
 import ThoughtCard from './ThoughtCard'
 
 
