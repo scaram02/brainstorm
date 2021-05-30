f733d0d6d8295b5665b1a09c3f106a159aacbd28
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-05-29 20:19:33 -0500

    client/build

---
+++ client/build/precache-manifest.5a21d1cbc6a6543275c7c2fc0a8cc8cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8e05ce5fe8b2ed8bb9c1d1da288e61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11fdc834189ff9e5117e",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0006c6800abb82d07ea7",
+    "url": "/static/js/2.e62a9924.chunk.js"
+  },
+  {
+    "revision": "11fdc834189ff9e5117e",
+    "url": "/static/js/main.285a5811.chunk.js"
+  },
+  {
+    "revision": "7c1a75624ce30fc349a1",
+    "url": "/static/js/runtime-main.0352dc6d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
